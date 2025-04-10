b7d1a8fdaef396cb2cb36b03ffbf45c1df6d05b9
Matt Elggren <10578693+mattelggren@users.noreply.github.com>
2025-04-09 18:44:02 -0700

    Adds tests for intro.js/fizzBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro.js";
+import { max, fizzBuzz } from "../src/intro.js";
 
 describe('max', () => {
     it('should return the first argument if it is greater', () => {
@@ -13,11 +13,26 @@ describe('max', () => {
         //expect(result).toBe(10);
         // OR, more concisely
         expect(max(10, 5)).toBe(10);
-    }),
+    });
     it('should return the second argument if it is greater', () => {
         expect(max(5, 10)).toBe(10);
     });
     it('should return the first argument if they are equal', () => {
         expect(max(10, 10)).toBe(10);
-    })
-})
+    });
+});
+
+describe('fizzBuzz', () => {
+    it('should return FizzBuzz if arg is divisible by 3 and 5', () => {
+        expect(fizzBuzz(30)).toBe('FizzBuzz');
+    });
+    it('should return Fizz if arg is divisible by 3 but not 5', () => {
+        expect(fizzBuzz(9)).toBe('Fizz');
+    });
+    it('should return Buzz if arg is divisible by 5 but not 3', () => {
+        expect(fizzBuzz(10)).toBe('Buzz');
+    });
+    it('should return the input as a string if arg is not divisible by 3 or 5', () => {
+        expect(fizzBuzz('NAN')).toBe('NAN');
+    });
+});
