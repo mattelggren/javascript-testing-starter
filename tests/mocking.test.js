1cb70dc68653d8a15109a4bcf7fcccdb5508457d
Matt Elggren <10578693+mattelggren@users.noreply.github.com>
2025-04-16 13:23:25 -0700

    Adds unit tests for currency.js/getPriceInCurrency and shipping.js/getShippingInfo

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,10 @@
 import { vi, it, expect, describe } from 'vitest'
+import { getPriceInCurrency, getShippingInfo } from '../src/mocking';
+import { getExchangeRate } from '../src/libs/currency';
+import { getShippingQuote } from '../src/libs/shipping';
+
+vi.mock('../src/libs/currency');
+vi.mock('../src/libs/shipping');
 
 describe('a test suite with sample mocks and mock matcher usage', () => {
     it('a test case for return value mocking', () => {
@@ -25,11 +31,38 @@ describe('a test suite with sample mocks and mock matcher usage', () => {
 });
 
 describe('mockSendText', () => {
-    it('should return confirmation of message sent', () => {
+    it('should return confirmation of a single message sent', () => {
         const sendText = vi.fn();
         sendText.mockImplementation(message => 'Message sent: ' + message);
         
         expect(sendText('yo, wassup')).toBe('Message sent: yo, wassup');
-        expect(sendText).toHaveBeenCalledOnce();
+        expect(sendText).toHaveBeenCalledExactlyOnceWith('yo, wassup');
+    });
+});
+
+describe('getPriceInCurrency', () => {
+    it('should return price in target currency equivalent of USD', () => {
+        vi.mocked(getExchangeRate).mockReturnValue(1.5);
+        
+        const price = getPriceInCurrency(10, 'AUD');
+
+        expect(price).toBe(15);
+    });
+});
+
+describe('getShippingInfo', () => {
+    it('should return shipping info if quote can be fetched', () => {
+        vi.mocked(getShippingQuote).mockReturnValue({ cost: 1, estimatedDays: 2 });
+        
+        const shippingInfo = getShippingInfo('Seattle');
+
+        expect(shippingInfo).toMatch(/shipping cost: \$1 \(2 days\)/i);
+    });
+    it('should return an error if quote cannot be fetched', () => {
+        vi.mocked(getShippingQuote).mockReturnValue(null);
+        
+        const shippingInfo = getShippingInfo('London');
+
+        expect(shippingInfo).toMatch(/unavailable/i);
     });
 });
