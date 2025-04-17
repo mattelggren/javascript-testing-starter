a93b2d45dd5acdf4d28825645828e8a32fad3e88
Matt Elggren <10578693+mattelggren@users.noreply.github.com>
2025-04-17 09:47:53 -0700

    Adds unit tests for mocking.js/isOnline, also fixes a defect in isOnline detected by the tests

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,5 +1,5 @@
 import { vi, it, expect, describe } from 'vitest'
-import { getPriceInCurrency, getShippingInfo, login, renderPage, submitOrder, signUp } from '../src/mocking';
+import { getPriceInCurrency, getShippingInfo, isOnline, login, renderPage, submitOrder, signUp } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
 import { trackPageView } from '../src/libs/analytics';
@@ -151,5 +151,22 @@ describe('login', () => {
         const securityCode = spy.mock.results[0].value.toString();
         
         expect(sendEmail).toHaveBeenCalledWith(email, securityCode);
+    });
+});
+
+describe('isOnline', () => {
+    it('should return false if current hour is outside open hours', () => {
+        vi.setSystemTime('2025-01-10 07:59');
+        expect(isOnline()).toBe(false);
+
+        vi.setSystemTime('2025-01-10 20:01');
+        expect(isOnline()).toBe(false);
+    });
+    it('should return true if current hour is within open hours', () => {
+        vi.setSystemTime('2025-01-01 8:00');
+        expect(isOnline()).toBe(true);
+
+        vi.setSystemTime('2025-01-01 19:59');
+        expect(isOnline()).toBe(true);
     });
 });
