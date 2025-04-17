48f323033331d2809b11e456a4b619e3b4fe80c5
Matt Elggren <10578693+mattelggren@users.noreply.github.com>
2025-04-16 17:08:59 -0700

    Adds unit tests for mocking.js/signUp

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,14 +1,22 @@
 import { vi, it, expect, describe } from 'vitest'
-import { getPriceInCurrency, getShippingInfo, renderPage, submitOrder } from '../src/mocking';
+import { getPriceInCurrency, getShippingInfo, renderPage, submitOrder, signUp } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
 import { trackPageView } from '../src/libs/analytics';
 import { charge } from '../src/libs/payment';
+import { sendEmail } from '../src/libs/email';
 
 vi.mock('../src/libs/currency');
 vi.mock('../src/libs/shipping');
 vi.mock('../src/libs/analytics');
 vi.mock('../src/libs/payment');
+vi.mock('../src/libs/email', async (importOriginal) => {
+    const originalModule = await importOriginal();
+    return {
+        ...originalModule,
+        sendEmail: vi.fn()
+    }
+});
 
 describe('a test suite with sample mocks and mock matcher usage', () => {
     it('a test case for return value mocking', () => {
@@ -105,5 +113,22 @@ describe('submitOrder', () => {
 
         submitOrder(order, creditCard).then(result => 
             expect(result).toEqual({success: false, error: 'payment_error'}));
+    });
+});
+
+describe('signUp', () => {
+    const email ='name@domain.com';
+    it('should return false if email is not valid', async () => {
+        signUp(null).then(result => expect(result).toBe(false));
+    });
+    it('should return true if email is valid', async () => {
+        signUp(email).then(result => expect(result).toBe(true));
+    });
+    it('should send welcome email if email is valid', async () => {
+        const args = vi.mocked(sendEmail).mock.calls[0];
+
+        expect(sendEmail).toHaveBeenCalled();
+        expect(args[0]).toBe(email);
+        expect(args[1]).toMatch(/welcome/i);
     });
 });
